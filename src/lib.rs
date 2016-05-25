29712f3bf0b8a339693b673d45ddd6104c955aba
Michael Younkin <younkin.michael@gmail.com>
2016-05-24 20:18:57 -0400

    Impl naive server with mio
    Mio seems like the best choice for non-blocking IO in Rust at this point
    (though it isn't as good as it could be).
    
    I'm just trying to get a feel for how to work with buffers and other stuff like
    that before delving in to the protocol implementation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,99 @@
-use std::net::UdpSocket;
+use std::net::SocketAddr;
+use std::error::Error;
 use std::thread;
+use std::collections::HashSet;
 
-fn do_server() {
-    let mut socket = UdpSocket::bind("127.0.0.1:10101").unwrap();
-    loop {
-        let mut buf = [0; 256];
-        match socket.recv_from(&mut buf) {
-            Ok((amt, src)) => {
-                println!("Server got message from {}", src);
-                socket.send_to("Hello from the server!".as_bytes(), src).unwrap();
-            },
-            _ => (),
-        };
+extern crate mio;
+use mio::udp::UdpSocket;
+
+const MAX_PACKET_SIZE: usize = 64000;
+const MESSAGE_PREFIX: &'static str = "RNETMSG";
+
+#[derive(PartialEq, Clone)]
+pub enum Event {
+    Connect(SocketAddr),
+    Disconnect(SocketAddr),
+    Receive(SocketAddr, Vec<u8>),
+}
+
+#[derive(PartialEq, Copy, Clone)]
+enum MessageType {
+    Connect,
+    Disconnect,
+    Receive,
+    Unknown
+}
+
+impl From<u8> for MessageType {
+    fn from(v: u8) -> MessageType {
+        match v {
+            0 => MessageType::Connect,
+            1 => MessageType::Disconnect,
+            2 => MessageType::Receive,
+            _ => MessageType::Unknown,
+        }
     }
 }
 
-fn do_client() {
-    let mut socket = UdpSocket::bind("127.0.0.1:10102").unwrap();
-    socket.send_to("Hello from the client!".as_bytes(), "127.0.0.1:10101").unwrap();
-    let mut buf = [0; 256];
-    let (amt, _) = socket.recv_from(&mut buf).unwrap();
-    let response = std::str::from_utf8(&buf[0..amt]).unwrap();
-    println!("Server said {}", response);
+struct Host {
+    addr: SocketAddr,
+    socket: UdpSocket,
+    clients: HashSet<SocketAddr>,
+}
+
+impl Host {
+    fn new(addr: SocketAddr) -> Result<Host, Box<Error>> {
+        Ok(Host {
+            addr: addr,
+            socket: try!(UdpSocket::bound(&addr)),
+            clients: HashSet::new(),
+        })
+    }
+
+    fn service(&mut self) -> Result<Vec<Event>, Box<Error>> {
+        let mut events = Vec::new();
+        let mut buf = Vec::with_capacity(MAX_PACKET_SIZE);
+
+        // TODO handle truncation errors (possibly with a panic)
+        while let Some((data_len, src)) = try!(self.socket.recv_from(&mut buf)) {
+            let prefix = &buf[0..MESSAGE_PREFIX.len()];
+            // Ignore anything that doesn't start with our prefix
+            if prefix != MESSAGE_PREFIX.as_bytes() {
+                continue;
+            }
+
+            let message_type = buf[MESSAGE_PREFIX.len()];
+            let message_payload = &buf[MESSAGE_PREFIX.len() + 1..data_len];
+            match From::from(message_type) {
+                MessageType::Connect => {
+                    if self.clients.contains(&src) {
+                        events.push(Event::Disconnect(src));
+                        events.push(Event::Connect(src));
+                    } else {
+                        self.clients.insert(src);
+                        events.push(Event::Connect(src));
+                    }
+                },
+                MessageType::Disconnect => {
+                    if self.clients.contains(&src) {
+                        self.clients.remove(&src);
+                        events.push(Event::Disconnect(src));
+                    } else {
+                        events.push(Event::Connect(src));
+                        events.push(Event::Disconnect(src));
+                    }
+                },
+                MessageType::Receive => {
+                    if self.clients.contains(&src) {
+                        events.push(Event::Receive(src, From::from(message_payload)));
+                    }
+                },
+                _ => ()
+            }
+        }
+
+        Ok(events)
+    }
 }
 
 #[test]
